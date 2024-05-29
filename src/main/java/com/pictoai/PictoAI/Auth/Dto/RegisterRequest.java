c1616e066500ecfa4726e8b48a5438e359024a1c
frakalva <u202020026@upc.edu.pe>
2024-05-29 00:42:51 -0500

    feat: login and register dao

---
+++ src/main/java/com/pictoai/PictoAI/Auth/Dto/RegisterRequest.java
@@ -0,0 +1,22 @@
+package com.pictoai.PictoAI.Auth.Dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest{
+    String email;
+    String password;
+    String firstname;
+    String lastname;
+
+}
